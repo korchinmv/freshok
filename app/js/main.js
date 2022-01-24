70759548d2e90768379b5fb7a9f5c15500839b5b
korchinmv <korchinmv@yandex.ru>
2022-01-25 00:14:02 +0300

    header 50%

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function () {
 
-
+  let flag = false;
+  $('.menu-dropdown').click(function () {
+    if (!flag) {
+      $('.menu-dropdown__item').slideDown();
+    } else {
+      $('.menu-dropdown__item').slideUp();
+    }
+    flag = !flag;
+  });
 });
