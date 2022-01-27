39254c31be2a32ce74ce947cf7fa9bb66964dd83
korchinmv <korchinmv@yandex.ru>
2022-01-27 23:04:55 +0300

    added footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function () {
   $('.hero-slider').slick({
-
+    // autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow: '<button type="button" class="slick-prev"><img src="/images/elements/prev-arrow.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="/images/elements/next-arrow.svg" alt=""></button>',
   });
 });
